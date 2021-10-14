fd489678c12732a3861ce3bb0c9714c93f6c31c4
陈永达 <411416311@qq.com>
2021-10-14 14:38:54 +0800

    BossRoom

---
+++ Assets/Mirror/Examples/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public uint netId;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        netId = GetComponent<Mirror.NetworkIdentity>().netId;
+    }
+}
